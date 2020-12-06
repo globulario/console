47b591fdd352c3bd523cd5f23c1236db402c8fc5
davecourtois <dave.courtois60@gmail.com>
2020-12-05 22:19:13 -0500

    side-menu

--- src/application.ts
+++ src/application.ts
@@ -4,14 +4,21 @@ import { ApplicationView } from "../../globular-mvc/ApplicationView";
 import { Model } from "../../globular-mvc/Model";
 import { Account } from "../../globular-mvc/Account";
 import { Dashboard } from "./dashboard/dashboard";
+import "../../globular-mvc/node_modules/@polymer/iron-selector/iron-selector"
 
 export class ConsoleApplicationView extends ApplicationView {
   private welcomeContent: string;
   private dashboard: Dashboard;
+  private navigationDiv: any;
 
   constructor() {
     super();
     this.welcomeContent = this.getWorkspace().innerHTML;
+
+    // Here I will create the navgation menu.
+
+
+
   }
 
   // init the view
@@ -24,6 +31,61 @@ export class ConsoleApplicationView extends ApplicationView {
     this.getWorkspace().innerHTML = "";
     this.dashboard = new Dashboard(this.getWorkspace());
     this.dashboard.init();
+
+    let navigationHtml = `
+    <style>
+
+      #topMenu {
+          display: flex;
+          flex-direction: column;
+          min-width: fit-content;
+          font-family: Roboto, Ubuntu, Arial, sans-serif;
+          font-size: 81.25%;
+      }
+
+
+      #topMenu a{
+          align-items: center;
+          color: var(--settings-nav-item-color);
+          display: flex;
+          font-weight: 500;
+          margin-inline-end: 2px;
+          margin-inline-start: 1px;
+          min-height: 20px;
+          padding-bottom: 10px;
+          padding-inline-start: 23px;
+          padding-top: 10px;
+      }
+
+    </style>
+
+    <div id="left">
+      <settings-menu>
+        <iron-selector id="topMenu" selectable="a:not(#extensionsLink)" attr-for-selected="href" role="navigation">
+          <a>Item 1</a>
+          <a>Item 2</a>
+          <a>Item 3</a>
+        </iron-selctor>
+      </settings-menu>
+    </div>
+
+    `
+
+    let range = document.createRange();
+    this.getSideMenu().appendChild(range.createContextualFragment(navigationHtml))
+    this.navigationDiv = document.getElementById("navigation-div")
+/*
+    console.log(this.navigationDiv.childNodes)
+    this.navigationDiv.childNodes[1].onclick = ()=>{
+      console.log("hello dashboard")
+    }
+
+    this.navigationDiv.childNodes[3].onclick = ()=>{
+      console.log("hello documentation")
+    }
+*/
+    // fire the window resize event to display the side menu.
+    window.dispatchEvent(new Event('resize'));
   }
 
   onLogout() {
