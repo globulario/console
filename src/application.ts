8f0daed9289895f0f3a70bd924459802e930afdb
davecourtois <dave.courtois60@gmail.com>
2020-12-20 17:30:54 -0500

    start social network functionality programming

--- src/application.ts
+++ src/application.ts
@@ -3,23 +3,33 @@ import { Application } from "../../globular-mvc/Application";
 import { ApplicationView } from "../../globular-mvc/ApplicationView";
 import { Model } from "../../globular-mvc/Model";
 import { Account } from "../../globular-mvc/Account";
+import { ServerReflectionRequest, ServerReflectionResponse} from "../../globular-mvc/node_modules/globular-web-client/reflection/reflection_pb"
+import { ServerReflectionPromiseClient} from "../../globular-mvc/node_modules/globular-web-client/reflection/reflection_grpc_web_pb"
+
 import "../../globular-mvc/node_modules/@polymer/iron-selector/iron-selector"
 
 export class ConsoleApplicationView extends ApplicationView {
   private welcomeContent: string;
   private navigationDiv: any;
+  public displayWelcomePage: ()=>void;
 
   constructor() {
     super();
     this.welcomeContent = this.getWorkspace().innerHTML;
 
     // Here I will create the navgation menu.
+    let rqst = new ServerReflectionRequest;
+
+    
+    rqst.setHost("localhost:10001")
+    let client = new ServerReflectionPromiseClient("localhost:10001")
     
   }
 
   onLogin(account: Account) {
     super.onLogin(account);
     this.getWorkspace().innerHTML = "";
+    console.log("-----> on login!");
     let navigationHtml = `
     <style>
 
@@ -49,15 +59,20 @@ export class ConsoleApplicationView extends ApplicationView {
 
     <div id="left">
       <settings-menu>
-        <iron-selector id="topMenu" selectable="a:not(#extensionsLink)" attr-for-selected="href" role="navigation">
+        <!--iron-selector id="topMenu" selectable="a:not(#extensionsLink)" attr-for-selected="href" role="navigation">
           <a>Item 1</a>
           <a>Item 2</a>
           <a>Item 3</a>
-        </iron-selctor>
+        </iron-selctor-->
       </settings-menu>
     </div>
 
     `
+
+    // Clear previous content.
+    if(document.getElementById("left") != undefined){
+      document.getElementById("left").innerHTML = ""
+    }
 
     let range = document.createRange();
     this.getSideMenu().appendChild(range.createContextualFragment(navigationHtml))
@@ -69,7 +84,8 @@ export class ConsoleApplicationView extends ApplicationView {
 
   onLogout() {
     super.onLogout();
-    this.getWorkspace().innerHTML = this.welcomeContent;
+    this.getWorkspace().innerHTML = "";
+    this.displayWelcomePage();
   }
 }
 
