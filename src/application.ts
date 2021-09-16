becfa90f69d76deff3f4bebd029b7e8c10a40fdd
davecourtois <dave.courtois60@gmail.com>
2021-09-16 19:51:15 -0400

    move settings to settings

--- src/application.ts
+++ src/application.ts
@@ -4,8 +4,6 @@ import { ApplicationView } from "../../globular-mvc/ApplicationView";
 import { Model } from "../../globular-mvc/Model";
 import { Account } from "../../globular-mvc/Account";
 import { SettingsMenu, SettingsPanel } from "../../globular-mvc/components/Settings"
-import { ServerGeneralSettings } from "./serverGeneralSettings";
-import { ServicesSettings } from "./servicesSettings";
 import { SaveConfigRequest } from "../../globular-mvc/node_modules/globular-web-client/admin/admin_pb"
 
 export class ConsoleApplicationView extends ApplicationView {
@@ -25,35 +23,9 @@ export class ConsoleApplicationView extends ApplicationView {
   onLogin(account: Account) {
     super.onLogin(account);
     this.getWorkspace().innerHTML = "";
-
-
     this.getSideMenu().appendChild(this.consoleSettingsMenu)
     this.getWorkspace().appendChild(this.consoleSettingsPanel)
-
-    // Now the save menu
-    let saveMenuItem = this.consoleSettingsMenu.appendSettingsMenuItem("save", "Save");
-    saveMenuItem.onclick = () => {
-      saveMenuItem.style.display = "none"
-      //ApplicationView.displayMessage("The server will now restart...", 3000)
-      let saveRqst = new SaveConfigRequest
-      saveRqst.setConfig(JSON.stringify(Model.globular.config))
-      Model.globular.adminService.saveConfig(saveRqst, {
-        token: localStorage.getItem("user_token"),
-        application: Model.application,
-        domain: Model.domain
-      }).then(() => { })
-        .catch(err => {
-          ApplicationView.displayMessage(err, 3000)
-        })
-    }
-
-    saveMenuItem.style.display = "none"
-    // Configuration menu...
-    let serverGeneralSettings = new ServerGeneralSettings(Model.globular.config, this.consoleSettingsMenu, this.consoleSettingsPanel, saveMenuItem);
-    let servicesSettings = new ServicesSettings(this.consoleSettingsMenu, this.consoleSettingsPanel, saveMenuItem);
-
-    this.consoleSettingsMenu.show()
-
+    
     // fire the window resize event to display the side menu.
     window.dispatchEvent(new Event('resize'));
   }
